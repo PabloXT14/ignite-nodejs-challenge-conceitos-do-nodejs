185d0ea3da0182e0f6801587fcc77bd8cc0a8bec
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-12-30 16:21:28 -0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header;
+
+  const user = users.find(user => user.username === username);
+
+  // Checando se usuário existe
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request = user;
+
+  next();
 }
 
+/* Rota de criação de usuário */
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  // Checando se username já existe
+  const userAlredyExists = users.find(user => user.username === username)
+
+  if (userAlredyExists) {
+    return response.status(400).json({ error: "This user alredy exists" });
+  }
+
+  // Criando novo usuário
+  const newUser = {
+    id: v4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
