b72fb902a35bbfaf6fc771b1281fee05f8a70ea3
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-01-03 15:26:49 -0300

    Challange finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, v4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -10,8 +10,9 @@ app.use(express.json());
 
 const users = [];
 
+// Customized Middlewares
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.header;
+  const { username } = request.headers;
 
   const user = users.find(user => user.username === username);
 
@@ -20,9 +21,9 @@ function checksExistsUserAccount(request, response, next) {
     return response.status(404).json({ error: "User not found" })
   }
 
-  request = user;
+  request.user = user;// enviando dado pelo request
 
-  next();
+  return next();// função para continuar o fluxo normal da aplicação
 }
 
 /* Rota de criação de usuário */
@@ -38,7 +39,7 @@ app.post('/users', (request, response) => {
 
   // Criando novo usuário
   const newUser = {
-    id: v4(),
+    id: uuidv4(),
     name: name,
     username: username,
     todos: []
@@ -50,24 +51,94 @@ app.post('/users', (request, response) => {
 
 });
 
+
+/* Rota para listar todos de um user específico */
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;//vem do Middleware
+
+  return response.json(user.todos);// retorna os todos do user
 });
 
+
+/* Rota para criação dos todos */
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;// vem do Middleware
+  const { title, deadline } = request.body;
+
+  const newTodo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),//obs: deve estar no formato ANO-MÊS-DIA
+    created_at: new Date()
+  }
+
+  // Add todo no array
+  user.todos.push(newTodo);
+
+  return response.status(201).json(newTodo);
 });
 
+
+/* Rota para atualizar dados de um todo */
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;//come from Middleware
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  // Checando se tarefa existe
+  const todoExists = user.todos.find(todo => todo.id === id);
+
+  if (!todoExists) {
+    return response.status(404).json({ error: "Todo not found!" })
+  }
+
+  //Alterando dados do todo
+  todoExists.title = title;
+  todoExists.deadline = new Date(deadline);
+
+  return response.status(200).json(todoExists);
 });
 
+
+/* Rota para colocar todo como feito */
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;//vem do Middleware
+  const { id } = request.params;
+
+  // Pegando todo especifico por id
+  const updatedTodo = user.todos.find(todo => todo.id === id);
+
+  // Checando se todo existe
+  if (!updatedTodo) {
+    return response.status(404).json({ error: "Todo not found!" })
+  }
+
+  // Atualizando campo done do todo
+  updatedTodo.done = true;
+
+  return response.status(200).json(updatedTodo);
+
 });
 
+
+/* Rota para deletar um todo */
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;//vem do Middleware
+  const { id } = request.params;
+
+  const todoExists = user.todos.find(todo => todo.id === id);
+
+  // Checando se todo existe
+  if (!todoExists) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  // Excluindo todo
+  user.todos.splice(todoExists, 1);
+
+  // Obs: status(204): representa uma resposta sem conteúdo.
+  return response.status(204).json();
 });
 
 module.exports = app;
